9179346779b3f5e654ac2a9bfa755c8f268d7571
Matteo Foglieni <matteo.foglieni16@gmail.com>
2022-07-08 16:58:18 +0200

    now tests

--- src/PPXiMatter.jl
+++ src/PPXiMatter.jl
@@ -17,7 +17,7 @@
 # along with GaPSE. If not, see <http://www.gnu.org/licenses/>.
 #
 
-function ξ_PP_Matter_L0(P::Point, cosmo::Cosmology)
+function ξ_PPGalaxies_L0(P::Point, cosmo::Cosmology)
      b = cosmo.params.b
      s = P.comdist
      
@@ -27,12 +27,12 @@ function ξ_PP_Matter_L0(P::Point, cosmo::Cosmology)
      (b^2 + 2 * b * f / 3 + f^2 / 5) * D^2 * cosmo.tools.I00(s)
 end
 
-function ξ_PP_Matter_L0(s1, cosmo::Cosmology)
+function ξ_PPGalaxies_L0(s1, cosmo::Cosmology)
      P1 = Point(s1, cosmo)
-     return ξ_PP_Matter_L0(P1, cosmo)
+     return ξ_PPGalaxies_L0(P1, cosmo)
 end
 
-function ξ_PP_Matter_L2(P::Point, cosmo::Cosmology)
+function ξ_PPGalaxies_L2(P::Point, cosmo::Cosmology)
      b = cosmo.params.b
      s = P.comdist
 
@@ -42,12 +42,12 @@ function ξ_PP_Matter_L2(P::Point, cosmo::Cosmology)
      -(4 * b * f / 3 + 4 * f^2 / 7) * D^2 * cosmo.tools.I20(s)
 end
 
-function ξ_PP_Matter_L2(s1, cosmo::Cosmology)
+function ξ_PPGalaxies_L2(s1, cosmo::Cosmology)
      P1 = Point(s1, cosmo)
-     return ξ_PP_Matter_L2(P1, cosmo)
+     return ξ_PPGalaxies_L2(P1, cosmo)
 end
 
-function ξ_PP_Matter_L4(P::Point, cosmo::Cosmology)
+function ξ_PPGalaxies_L4(P::Point, cosmo::Cosmology)
      s = P.comdist
 
      Peff = Point(cosmo.s_eff, cosmo)
@@ -56,14 +56,14 @@ function ξ_PP_Matter_L4(P::Point, cosmo::Cosmology)
      8 * f^2 / 35 * D^2 * cosmo.tools.I40(s)
 end
 
-function ξ_PP_Matter_L4(s1, cosmo::Cosmology)
+function ξ_PPGalaxies_L4(s1, cosmo::Cosmology)
      P1 = Point(s1, cosmo)
-     return ξ_PP_Matter_L4(P1, cosmo)
+     return ξ_PPGalaxies_L4(P1, cosmo)
 end
 
-function ξ_PP_Matter(s1, y, cosmo::Cosmology)
-     return ξ_PP_Matter_L0(s1, cosmo) + ξ_PP_Matter_L2(s1, cosmo) * Pl(y, 2) + 
-          ξ_PP_Matter_L4(s1, cosmo) * Pl(y, 4)
+function ξ_PPGalaxies(s1, y, cosmo::Cosmology)
+     return ξ_PPGalaxies_L0(s1, cosmo) + ξ_PPGalaxies_L2(s1, cosmo) * Pl(y, 2) + 
+          ξ_PPGalaxies_L4(s1, cosmo) * Pl(y, 4)
 end
 
 
@@ -72,20 +72,20 @@ end
 
 
 
-function integrand_ξ_PPMatter_multipole(s, μ, cosmo::Cosmology;
+function integrand_ξ_PPGalaxies_multipole(s, μ, cosmo::Cosmology;
      L::Int=0, use_windows::Bool=true)
 
      res = if use_windows == true
           #println("s1 = $s1 \\t s2 = $(s2(s1, s, μ)) \\t  y=$(y(s1, s, μ))")
           #int = cosmo.ξ_matter(s)
-          int = ξ_PP_Matter(s, μ, cosmo)
+          int = ξ_PPGalaxies(s, μ, cosmo)
           #println("int = $int")
           #coef = (cosmo.params.b + cosmo.f_of_s(s) * μ^2)^2 * cosmo.D_of_s(s)
           #println("coef = $coef")
           int .* (spline_integrF(s, μ, cosmo.windowFint) / cosmo.WFI_norm * Pl(μ, L))
      else
           #int = cosmo.ξ_matter(s)
-          int = ξ_PP_Matter(s, μ, cosmo)
+          int = ξ_PPGalaxies(s, μ, cosmo)
           #coef = (cosmo.params.b + cosmo.f_of_s(s) * μ^2)^2
           int .* Pl(μ, L)
      end
@@ -100,7 +100,7 @@ end
 
 
 
-function ξ_PPMatter_multipole(
+function ξ_PPGalaxies_multipole(
      s, cosmo::Cosmology;
      L::Int=0,
      use_windows::Bool=true,
@@ -108,7 +108,7 @@ function ξ_PPMatter_multipole(
      μ_atol::Float64=0.0,
      μ_rtol::Float64=1e-2)
 
-     orig_f(μ) = enhancer * integrand_ξ_PPMatter_multipole(s, μ, cosmo;
+     orig_f(μ) = enhancer * integrand_ξ_PPGalaxies_multipole(s, μ, cosmo;
           L=L, use_windows=use_windows)
 
      int = quadgk(μ -> orig_f(μ), -1.0, 1.0; atol=μ_atol, rtol=μ_rtol)[1]
@@ -121,7 +121,7 @@ end
 
 
 
-function map_ξ_PPMatter_multipole(cosmo::Cosmology,
+function map_ξ_PPGalaxies_multipole(cosmo::Cosmology,
      v_ss=nothing;
      pr::Bool=true,
      N_log::Int=1000,
@@ -131,15 +131,15 @@ function map_ξ_PPMatter_multipole(cosmo::Cosmology,
      t1 = time()
      ss = isnothing(v_ss) ? 10 .^ range(0, 3, length=N_log) : v_ss
      xis = pr ? begin
-          @showprogress "PP Matter, L=$L: " [
-               ξ_PPMatter_multipole(s, cosmo; L=L, kwargs...) for s in ss
+          @showprogress "PP Galaxies, L=$L: " [
+               ξ_PPGalaxies_multipole(s, cosmo; L=L, kwargs...) for s in ss
           ]
      end : [
-          ξ_PPMatter_multipole(s, cosmo; L=L, kwargs...) for s in ss
+          ξ_PPGalaxies_multipole(s, cosmo; L=L, kwargs...) for s in ss
      ]
 
      t2 = time()
-     pr && println("\ntime needed for map_ξ_PPMatter_multipole " *
+     pr && println("\ntime needed for map_ξ_PPGalaxies_multipole " *
                    "[in s] = $(@sprintf("%.5f", t2-t1)) ")
      return (ss, xis)
 end
@@ -149,21 +149,21 @@ end
 
 
 
-function print_map_ξ_PPMatter_multipole(
+function print_map_ξ_PPGalaxies_multipole(
      cosmo::Cosmology,
      out::String,
      v_ss=nothing;
      kwargs...)
 
      t1 = time()
-     vec = map_ξ_PPMatter_multipole(cosmo, v_ss; kwargs...)
+     vec = map_ξ_PPGalaxies_multipole(cosmo, v_ss; kwargs...)
      t2 = time()
 
      isfile(out) && run(`rm $out`)
      open(out, "w") do io
           println(io, BRAND)
 
-          println(io, "# This is an integration map on mu of the ξ multipole concerning the PP Matter.")
+          println(io, "# This is an integration map on mu of the ξ multipole concerning the PP Galaxies.")
           parameters_used(io, cosmo; logo = false)
           println(io, "# computational time needed (in s) : $(@sprintf("%.4f", t2-t1))")
           print(io, "# kwards passed: ")
