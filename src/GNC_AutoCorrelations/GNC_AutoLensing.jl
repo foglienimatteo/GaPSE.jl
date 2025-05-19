40e22624dbec80da89bce7e0adf96d8a3a07800e
Matteo Foglieni <matteo.foglieni16@gmail.com>
2025-05-19 13:58:31 +0200

    wip

--- src/GNC_AutoCorrelations/GNC_AutoLensing.jl
+++ src/GNC_AutoCorrelations/GNC_AutoLensing.jl
@@ -460,7 +460,7 @@ function ξ_GNC_Lensing(P1::Union{Point,DP}, P2::Union{Point,DP}, y, cosmo::Cosm
         IP1s = adapt(backend, [adapt(backend, GaPSE.Point(x, cosmo)) for x in χ1s])
         IP2s = adapt(backend, [adapt(backend, GaPSE.Point(x, cosmo)) for x in χ2s])
         devIP1, devIP2 =  adapt(backend,P1), adapt(backend,P2)
-        int_f(IP1, IP2, devIP1, devIP2, y, devcosmo) = 0.0f0 #integrand_ξ_GNC_Lensing(IP1, IP2, devIP1, devIP2, y, devcosmo)
+        int_f(IP1, IP2, devIP1, devIP2, y, devcosmo) = integrand_ξ_GNC_Lensing(IP1, IP2, devIP1, devIP2, y, devcosmo)
 
         @kernel function mykernel!(int_f, int_ξs, IP1s, IP2s, devIP1, devIP2, y, devcosmo)
             i, j = @index(Global, NTuple)
