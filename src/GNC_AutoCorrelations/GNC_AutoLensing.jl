1c1f8695625ed80430b21ed620ae12e168480e44
Matteo Foglieni <matteo.foglieni16@gmail.com>
2025-06-09 16:56:30 +0200

    LENSING WRONG: wip

--- src/GNC_AutoCorrelations/GNC_AutoLensing.jl
+++ src/GNC_AutoCorrelations/GNC_AutoLensing.jl
@@ -235,11 +235,11 @@ function ξ_GNC_Lensing(P1::Union{Point,DP}, P2::Union{Point,DP}, y, cosmo::Cosm
         #end
 
         #devcosmo = adapt(backend, cosmo)
-        #IP1s = adapt(backend, [adapt(backend, GaPSE.Point(x, cosmo)) for x in χ1s])
-        #IP2s = adapt(backend, [adapt(backend, GaPSE.Point(x, cosmo)) for x in χ2s])
-        #devIP1, devIP2 =  adapt(backend,P1), adapt(backend,P2)
+        devIP1s = adapt(backend, [adapt(backend, GaPSE.Point(x, cosmo)) for x in χ1s])
+        devIP2s = adapt(backend, [adapt(backend, GaPSE.Point(x, cosmo)) for x in χ2s])
+        devP1, devP2 =  adapt(backend,P1), adapt(backend,P2)
         #int_f(IP1, IP2, devIP1, devIP2, y, devcosmo) = integrand_ξ_GNC_Lensing(IP1, IP2, devIP1, devIP2, y, devcosmo)
-        IP1s, IP2s = [GaPSE.Point(x, cosmo) for x in χ1s], [GaPSE.Point(x, cosmo) for x in χ2s]
+        #IP1s, IP2s = [GaPSE.Point(x, cosmo) for x in χ1s], [GaPSE.Point(x, cosmo) for x in χ2s]
 
         ###@kernel function mykernel!(int_f, int_ξs, IP1s, IP2s, devIP1, devIP2, y, devcosmo)
         ###    i, j = @index(Global, NTuple)
@@ -253,7 +253,7 @@ function ξ_GNC_Lensing(P1::Union{Point,DP}, P2::Union{Point,DP}, y, cosmo::Cosm
 
         #kernel!(int_ξs, GaPSE.integrand_ξ_GNC_Lensing, IP1s, IP2s, P1, P2, y, cosmo, kwargs...; ndrange=size(int_ξs))
         compiled_kernel! = kernel_2d!(backend, 64)
-        compiled_kernel!(int_ξs, GaPSE.integrand_ξ_GNC_Lensing, IP1s, IP2s, P1, P2, y, devcosmo; ndrange=size(int_ξs))
+        compiled_kernel!(int_ξs, GaPSE.integrand_ξ_GNC_Lensing, devIP1s, devIP2s, devP1, devP2, y, devcosmo; ndrange=size(int_ξs))
         KernelAbstractions.synchronize(backend)
         hostint_ξs = Array(int_ξs)
 
